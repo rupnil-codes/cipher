a0d0b08bb50b2cbfc8674526d5e122849879286a
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-03-15 23:36:21 +0530

    (v0.1.4-alpha) Update Landing Page
    Im like 40% sure where I'm heading with this :sob: Anyways I added a big ahh heading and a small heading below. Added a blinking cursor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 
 ## TODOS:
 
-- [ ] Fix logo and make it PERFECT 
+- [ ] Fix logo and make it PERFECT.
+- [ ] When the user presses enter it may remove all the text (typewriter) and then do something.
 
 ## CHANGELOGS:
--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,10 +1,15 @@
 import "../styles/routes/landing.css";
 
 function Landing() {
+
     return (
         <>
             <div className={"landing"}>
-                <h1>$ C1PH<span className={"sigma-font"}>Σ</span>R_</h1>
+                <h1>C1PH<span className={"sigma-font"}>Σ</span>R</h1>
+                <h2>
+                    press ENTER to continue
+                    <span className={"cursor"}> █</span>
+                </h2>
             </div>
         </>
     )
