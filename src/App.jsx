fd36a1602862879b13bdc07bec616cebbfe60288
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-03-15 23:09:24 +0530

    (v0.1.3-alpha) Font + Colors
    Added new fonts and a color pallete in `variables.css`. I finally have a direction on what i am going to do! Just set up this siimple plain site for now :pf:

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-        <div className="app">
+        <div className="viewport">
 
             <Landing />
 
--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -3,6 +3,9 @@ import "../styles/routes/landing.css";
 function Landing() {
     return (
         <>
+            <div className={"landing"}>
+                <h1>$ C1PH<span className={"sigma-font"}>Σ</span>R_</h1>
+            </div>
         </>
     )
 }
